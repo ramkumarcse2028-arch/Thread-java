cd748daab1798eb53073d49702fc5299302cf39b
ramkumarcse2028-arch <ramkumarcse2028@gmail.com>
2026-09-14 18:12:27 +0530

    Add files via upload

---
+++ Thread_communicationDemo.java
@@ -0,0 +1,49 @@
+public class Thread_communicationDemo {
+    public static void main(String[] args) {
+        Box box = new Box();
+
+        Thread t1 = new Thread(() -> {
+            for(int i = 1; i<=20; i++){
+                try{
+                    Thread.sleep(100);
+                } catch( Exception e){}
+                box.producer(i);
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for(int i = 1; i<=20; i++){
+                try{
+                    Thread.sleep(100);
+                } catch( Exception e){}
+                box.consumer();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
+class Box {
+    volatile Integer item;
+    volatile Boolean flag = false;
+
+    void producer(int value){
+        while(flag == true ){
+            // do nothing 
+        }
+        item = value;
+        flag = true;
+        System.out.println("Producer produces " + item );
+    }
+
+    void consumer(){
+        while(flag == false){
+            // do nothing 
+        }
+        System.out.println("Consumer consumes " + item );
+        item = null;
+        flag = false;
+    }
+}
+
